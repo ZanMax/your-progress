8ef64dddd65dc987471ef9297078b0f996c9c828
maxim <zanoga.maxim@gmail.com>
2022-09-08 19:07:51 -0700

    finished draw chart

--- progress/utils.py
+++ progress/utils.py
@@ -15,24 +15,37 @@ def create_chart(commits):
     now = timezone.now()
     start = now - timezone.timedelta(days=364)
     daterange = date_range(start, now)
-    counts = [[] for _ in range(7)]
-    dates = [[] for _ in range(7)]
-    for dt in daterange:
-        count = commits.filter(created__date=dt).count()
-        day_number = dt.weekday()
-        counts[day_number].append(count)
-        dates[day_number].append(dt)
 
-    days = list(calendar.day_name)
+    projects_list = []
+    for c in commits:
+        if c.project.name not in projects_list:
+            projects_list.append(c.project.name)
 
-    fig = px.imshow(counts,
-                    color_continuous_scale="greens",
-                    x=dates[0],
-                    y=days,
-                    height=280,
-                    width=1200
-                    )
+    projects_chart = {}
+    for project in projects_list:
+        counts = [[] for _ in range(7)]
+        dates = [[] for _ in range(7)]
+        for dt in daterange:
+            count = commits.filter(created__date=dt).filter(project__name=project).count()
+            day_number = dt.weekday()
+            counts[day_number].append(count)
+            dates[day_number].append(dt)
 
-    fig.update_traces({"xgap": 5, "ygap": 5})
-    chart = fig.to_html()
-    return chart
+        days = list(calendar.day_name)
+
+        fig = px.imshow(counts,
+                        color_continuous_scale="greens",
+                        x=dates[0],
+                        y=days,
+                        height=280,
+                        width=1200
+                        )
+
+        fig.update_traces({"xgap": 5, "ygap": 5})
+        chart = fig.to_html(full_html=False, include_plotlyjs="cdn",
+                            config={"displayModeBar": False})
+        chart = chart.replace("\n", "")
+        chart = chart.replace("'", "")
+        projects_chart[project] = chart
+
+    return projects_chart
--- progress/views.py
+++ progress/views.py
@@ -1,56 +1,12 @@
-import calendar
 from django.shortcuts import render
-from django.utils import timezone
-
 from django.views import View
 
 from progress.models import Commit
-from progress.utils import date_range
-import plotly.express as px
 from progress.utils import create_chart
 
 
 class Progress(View):
     def get(self, request):
         commits = Commit.objects.filter(user=request.user).order_by("created").select_related("project").all()
-
-        project_commits = {}
-
-        for c in commits:
-            if c.project.name not in project_commits:
-                project_commits[c.project.name] = []
-            # project_commits[c.project.name].append(c)
-            project_commits[c.project.name].append(c.created)
-
-        projects_chart = {}
-        for project, commits in project_commits.items():
-            print(project)
-            projects_chart[project] = create_chart(commits)
-        """
-        now = timezone.now()
-        start = now - timezone.timedelta(days=364)
-        daterange = date_range(start, now)
-        counts = [[] for _ in range(7)]
-        dates = [[] for _ in range(7)]
-        for dt in daterange:
-            count = commits.filter(created__date=dt).count()
-            day_number = dt.weekday()
-            counts[day_number].append(count)
-            dates[day_number].append(dt)
-
-        days = list(calendar.day_name)
-
-        fig = px.imshow(counts,
-                        color_continuous_scale="greens",
-                        x=dates[0],
-                        y=days,
-                        height=280,
-                        width=1200
-                        )
-
-        fig.update_traces({"xgap": 5, "ygap": 5})
-        chart = fig.to_html()
-        chart2 = fig.to_html()
-        """
-
-        return render(request, "progress.html", {'projects': projects_chart.items()})
+        projects_chart = create_chart(commits)
+        return render(request, "progress.html", {'projects': projects_chart})
